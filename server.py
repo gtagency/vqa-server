230ad0295f5f96210c6c4ae41aef2e1749706ead
yatyrio <abhinavk99@gmail.com>
2018-03-06 18:25:03 -0500

    Add uuid handler

--- server.py
+++ server.py
@@ -20,10 +20,10 @@ class handle(BaseHTTPRequestHandler):
         x = self.rfile.read(content_len)
 
         uuid,img = x.split(b';')
-        uuid = int(uuid.decode('ascii'))
+        uuid = (uuid.decode('ascii'))
 
         img = bs.b64decode(img)
-
+        img = imgprocess(img)
         self.c.execute('insert into imag (uuid, img) values(?, ?)',(uuid,memoryview(img)))
         self.send_response(200)
         self.end_headers()
@@ -39,13 +39,22 @@ class handle(BaseHTTPRequestHandler):
         self.end_headers()
 
         x = parse_qs(self.path)
-        uuid = int(x['/?uuid'][0])
-        q = x['q'][0]
-        self.c.execute('select img from imag where uuid=?', (uuid,))
-        img = self.c.fetchone()[0]
-        img = np.asarray(Image.open(io.BytesIO(img)))
-        self.wfile.write(f'{model(img,q)}'.encode('ascii'))
-
+        if '/?uuid' in list(x.keys()) and 'q' in list(x.keys()) and len(x.keys()) == 2:
+            
+            uuid = (x['/?uuid'][0])
+            q = x['q'][0]
+            self.c.execute('select img from imag where uuid=?', (uuid,))
+            img = self.c.fetchone()[0]
+            img = np.asarray(Image.open(io.BytesIO(img)))
+            self.wfile.write(f'{model(img,q)}'.encode('ascii'))
+        elif list(x.keys()) == ['/?uuid']:
+            try:
+                self.c.execute('select img from imag where uuid=?', (uuid,))
+                self.wfile.write('True'.encode('ascii'))
+            except:
+                self.wfile.write('False'.encode('ascii'))
+        else:
+            self.wfile.write('What are you doing'.encode('ascii'))
 
 def run(server_class=HTTPServer, handler_class=handle):
     """
@@ -55,6 +64,8 @@ def run(server_class=HTTPServer, handler_class=handle):
     httpd = server_class(server_address, handler_class)
     print('Starting server')
     httpd.serve_forever()
+def imgprocess(image):
+    return 0
 def model(image, q):
     """
     Placeholder model function
