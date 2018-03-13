2d7e578939850d02f77974eaae5f711facf4f4a8
igolod <igolod@live.com>
2018-03-13 19:10:22 -0400

    Bug fixes

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import sqlite3 as sql
 from urllib.parse import urlparse, parse_qs
 import os
 import base64 as bs
-
+import sys
 
 class handle(BaseHTTPRequestHandler):
     conn = sql.connect('image.db')
@@ -21,13 +21,15 @@ class handle(BaseHTTPRequestHandler):
 
         uuid,img = x.split(b';')
         uuid = (uuid.decode('ascii'))
-
+        
         img = bs.b64decode(img)
-        img = imgprocess(img)
-        self.c.execute('insert into imag (uuid, img) values(?, ?)',(uuid,memoryview(img)))
+
+        params = (uuid,memoryview(img))
+        self.c.execute('insert into images values(?, ?)', params)
         self.send_response(200)
         self.end_headers()
-        dat = self.c.execute('select * from imag;')
+        dat = self.c.execute('select * from images;')
+
         self.conn.commit()
 
 
@@ -43,15 +45,17 @@ class handle(BaseHTTPRequestHandler):
             
             uuid = (x['/?uuid'][0])
             q = x['q'][0]
-            self.c.execute('select img from imag where uuid=?', (uuid,))
+            self.c.execute('select img from images where uuid=?', (uuid,))
             img = self.c.fetchone()[0]
             img = np.asarray(Image.open(io.BytesIO(img)))
             self.wfile.write(f'{model(img,q)}'.encode('ascii'))
         elif list(x.keys()) == ['/?uuid']:
-            try:
-                self.c.execute('select img from imag where uuid=?', (uuid,))
+            uuid = x['/?uuid'][0]
+            
+            j = self.c.execute('select img from images where uuid=?', (uuid,))
+            if len([i for i in j]) != 0:
                 self.wfile.write('True'.encode('ascii'))
-            except:
+            else:
                 self.wfile.write('False'.encode('ascii'))
         else:
             self.wfile.write('What are you doing'.encode('ascii'))
@@ -60,17 +64,16 @@ def run(server_class=HTTPServer, handler_class=handle):
     """
     Runs server at port 80
     """
-    server_address = ('', 80)
+    server_address = ('', 81)
     httpd = server_class(server_address, handler_class)
     print('Starting server')
     httpd.serve_forever()
-def imgprocess(image):
-    return 0
+
 def model(image, q):
     """
     Placeholder model function
     """
-
+    
     return 0
 if __name__ == '__main__':
     run()
